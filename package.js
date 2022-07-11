6946c6506a0f3e99528d262f04cc9ef684231ae0
stillLucy <101994919+stillLucy@users.noreply.github.com>
2022-07-11 14:53:14 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "form-project",
+  "private": true,
+  "version": "0.0.0",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "devDependencies": {
+    "eslint": "^8.19.0",
+    "prettier-eslint": "^15.0.1",
+    "vite": "^2.9.9"
+  },
+  "dependencies": {
+    "eslint-config-prettier": "^8.5.0"
+  }
+}
